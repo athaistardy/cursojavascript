457bd53b28efdd53b2ba46dbdd37fa24f2d50a34
thaistardy <thaistardy.s@gmail.com>
2021-02-13 16:34:24 -0300

    Adicionando pasta para criar Desafios propostos para o Guanabara e Adicionando o d001

---
+++ desafios/d001/script.js
@@ -0,0 +1,12 @@
+function clicou01() {
+    alert('Você Clicou no 1º Botão!')
+}
+
+function clicou02() {
+    alert('Você Clicou no 2º Botão!')
+}
+
+function clicou03() {
+    alert('Você Clicou no 3º Botão!')
+}
+
